2a74abf5a9a121b5a4d919bf1e3cbe41c9412e28
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-01-31 09:06:49 -0500

    0.0.1

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,6 +9,25 @@ export default function Header(){
                                 <img src="" alt="Facebook" />
                             </a>
                         </li>
+                        <li>
+                            <a href="#" title="Instagram">
+                                <img src="" alt="Instagram" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="TikTok">
+                                <img src="" alt="TikTok" />
+                            </a>
+                        </li>
+                    </ul>
+                    
+                    <ul>
+                        <li>
+                            <a href="">
+                                <img src="" alt="" />
+                                <h2>998877665</h2>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
