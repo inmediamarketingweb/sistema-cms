6d64a101b405cffd8e84007e099dd98cd4169353
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-02-03 11:38:01 -0500

    0.0.4

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,24 +1,57 @@
-import GlobalVariables from '../../GlobalVariables.json';
+import React, { useState, useEffect } from "react";
+import GlobalVariables from "../../GlobalVariables.json";
+import "./Header.css";
 
-import './Header.css';
+const Header = () => {
+  const [menuItems, setMenuItems] = useState([]);
 
-export default function Header(){
-    return (
-        <header>
-            <div className="header-top-container">
-                <div className='header-top'>
-                    <p>{GlobalVariables.HeaderTopText}</p>
-                    <a href={GlobalVariables.HeaderTopLink} title={GlobalVariables.HeaderTopLink}>Aquí</a>
-                </div>
-            </div>
+  useEffect(() => {
+    fetch("/Header.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Error al cargar el menú");
+            }
+            return response.json();
+        })
+        .then((data) => setMenuItems(data.PrincipalMenu))
+        .catch((error) => console.error("Error:", error));
+}, []);
 
-            <div className="header-bottom-container">
-                <div className="header-bottom">
-                    <a href="/" className='header-bottom-logo'>
-                        <img src="https://www.kamas.pe/img/logo-principal-kamas.webp" alt="Logo de Kamas"/>
+  return (
+    <header>
+      <div className="header-top-container">
+        <div className="header-top">
+          <p>{GlobalVariables.HeaderTopText}</p>
+          <a href={GlobalVariables.HeaderTopLink} title={GlobalVariables.HeaderTopLink}>
+            Aquí
+          </a>
+        </div>
+      </div>
+
+      <div className="header-bottom-container">
+        <div className="header-bottom">
+          <a href="/" className="header-bottom-logo">
+            <img src="https://www.kamas.pe/img/logo-principal-kamas.webp" alt="Logo de Kamas" />
+          </a>
+
+          <div>
+            <form></form>
+            <nav>
+              <ul>
+                {menuItems.map((item) => (
+                  <li key={item.id}>
+                    <a href={item.href}>
+                      <h2>{item.text}</h2>
                     </a>
-                </div>
-            </div>
-        </header>
-    );
-}
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
